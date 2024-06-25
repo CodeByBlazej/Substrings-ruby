dac433bef246c9626accf832f6d15123adc0ca73
CodeByBlazej <codebyblazej@gmail.com>
2024-06-25 18:23:59 +0100

    add method and dictionary

---
+++ app.rb
@@ -0,0 +1,8 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings (str, dictionary)
+
+end
+
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
