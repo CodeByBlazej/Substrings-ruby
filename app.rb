b301946be0e1bf58df52bb9fd3d54a2408332862
CodeByBlazej <codebyblazej@gmail.com>
2024-06-26 18:50:02 +0100

    add loops that return matching words

--- app.rb
+++ app.rb
@@ -1,16 +1,28 @@
+require 'pry-byebug'
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings (str, dictionary)
+  
+  str_words = str.downcase.split()
+  binding.pry
+  
+  dictionary.each do |element|
+    str_words.each do |word|
+      if element.include?(word)
+       p word
+      end
+  end
 
-  str_words = str.split()
-
-  # dictionary.reduce(Hash.new(0)) do |result, word|
-  #   str_words.each do |index|
-  #     if word.include?(index)
-  #       result[word] += 1
+  # dictionary.reduce(Hash.new(0)) do |result, value|
+  #   str_words.each do |word|
+  #     if dictionary.include?(word)
+  #       p word
+  #       # result[value] = value
+  #       # result
   #     end
   #   end
-  # end
+  end
 
 
 end
