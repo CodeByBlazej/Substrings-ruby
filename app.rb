475806c27470d27846bf5b7bf3a2e22625dadad5
CodeByBlazej <codebyblazej@gmail.com>
2024-06-27 17:44:26 +0100

    add new array with matching words

--- app.rb
+++ app.rb
@@ -6,14 +6,17 @@ def substrings (str, dictionary)
   
   str_words = str.downcase.split()
   binding.pry
+
+  matching_words = []
   
   dictionary.each do |element|
     str_words.each do |word|
       if element.include?(word)
-       p word
+        matching_words.push(word)
       end
   end
 
+  p matching_words
   # dictionary.reduce(Hash.new(0)) do |result, value|
   #   str_words.each do |word|
   #     if dictionary.include?(word)
